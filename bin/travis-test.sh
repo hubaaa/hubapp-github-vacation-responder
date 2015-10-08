f17fab68aad692dbed4e4fc01975b576b3e63502
Ronen Babayoff <ronen@lavaina.com>
2015-10-08 17:28:58 -0400

    Fix travis test script

--- bin/travis-test.sh
+++ bin/travis-test.sh
@@ -2,4 +2,4 @@
 
 # In travis, the meteor settings need to be added to travis as a private METEOR_SETTINGS environment variable,
 # that includes the json of your meteor settings file.
-spacejam test-packages --driver-package=practicalmeteor:mocha-console-reporter packages/*
+spacejam test-packages --driver-package=practicalmeteor:mocha-console-reporter app/packages/*
